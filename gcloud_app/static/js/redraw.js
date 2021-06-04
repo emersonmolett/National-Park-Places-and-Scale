579c53c700802c217c661027a352cb9585e20891
Emerson Molett <emerson.molett@gmail.com>
2021-06-04 18:40:25 -0500

    worked on various files in order to host in the cloud

---
+++ gcloud_app/static/js/redraw.js
@@ -0,0 +1,12 @@
+console.log("redraw.js loaded");
+
+// function markerOnClick(e) {
+//     alert(e.latlng);
+// };
+
+function on(PopupEvent) {
+    alert(`clicked ${latlng.lat}, ${latlng.long}`);
+    console.log(`clicked ${latlng.lat}, ${latlng.long}`);
+};
+
+console.log("redraw.js finished");
